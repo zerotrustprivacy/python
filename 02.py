7d42245adcd3f20cd0bb480edecb014f465f76a1
hendersonmersedes <78822295+hendersonmersedes@users.noreply.github.com>
2021-09-04 19:50:27 -0500

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,8 @@
+print("this program calculates the averages of two numbers.")
+print("the numbers are 4 and 8")
+print("the average is:",(4+5))
+age=32
+print(age)
+number1=4
+number2=8
+print("the number are" number1, "and" number2)
