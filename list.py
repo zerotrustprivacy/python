8528b4d249ef1cab5b4b760efa75075ed9a99a63
hendersonmersedes <78822295+hendersonmersedes@users.noreply.github.com>
2021-09-08 17:44:57 -0500

    Create list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+# This explains lists
+
+# Create a list variable
+mylist = []
+# now you're going to add or append to the list
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append(4)
+mylist.append(5)
+# print the list
+print(mylist)
