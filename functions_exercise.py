f1c04a8c26370288341b86b5277ba54836ddf39f
Mersedes Henderson <78822295+hendersonmersedes@users.noreply.github.com>
2022-03-25 10:46:32 -0500

    Create functions_exercise.py

---
+++ functions_exercise.py
@@ -0,0 +1,8 @@
+# Functions are blocks of code designed to perform a particular task
+#Start by defining the function then pass information to it
+
+def hello_user(username):
+  print(f"Hey, {username.title()}!")
+  
+#Now when you call hello_user, you can input a name and it displays a greeting
+  
